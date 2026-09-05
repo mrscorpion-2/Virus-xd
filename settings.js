faa728706a5c4bcb986d4bb19358680d24498a21
MR LWAZI.Z <mrscorpion204@gmail.com>
2026-09-06 00:20:28 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '27736324314',
+    botName: '24/7-XD',
+    ownerName: '24/7-XD'
+};
